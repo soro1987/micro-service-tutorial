f2077b29756c80bce55e86c893b8873bf7a33db3
soro1987 <72027471+soro1987@users.noreply.github.com>
2021-12-05 17:59:26 +0100

    Api gateway added

---
+++ ApiGateway/src/test/java/com/soro/photoapp/api/getaway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soro.photoapp.api.getaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppApiAccountManagement/src/test/java/com/soro/api/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
