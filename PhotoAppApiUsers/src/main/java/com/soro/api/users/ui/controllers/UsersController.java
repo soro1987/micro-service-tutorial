11c580b2cc4e9a762a45f511d7c9c636ccd40aaf
soro1987 <72027471+soro1987@users.noreply.github.com>
2021-12-07 09:21:26 +0100

    h2 database and spring security added

--- PhotoAppApiUsers/src/main/java/com/soro/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/soro/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,9 @@
 package com.soro.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status/check")
     public String status()
     {
-        return "Working ";
+//        return "Working on port ";
+
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping()
+    public String createUser()
+    {
+        return "Create user";
     }
 
 }
